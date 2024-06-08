3c156a42471c19f0622856d0b65b8d7d7c0767ee
Suresh1129 <2020cs026@stu.ucsc.cmb.ac.lk>
2024-06-08 12:09:31 +0530

    bg image set

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,12 +2,20 @@
 import { Button } from "@mui/material"
 import Link from "next/link"
 import Image from "next/image"
+import { Fullscreen } from "lucide-react"
 
 export default function Hero() {
     return (
-        <section className="w-full flex justify-center items-center py-5 md:py-5 lg:py-32 bg-[url('/images/project.jpg')] bg-cover bg-center">
-
-            <div className="container px-4 md:px-6 flex flex-col items-center justify-center text-center space-y-6">
+        <section className="w-full flex justify-center items-center py-5 md:py-5 lg:py-32  bg-cover bg-center relative">
+            <div>
+                <Image
+                    src="/images/project.jpg"
+                    alt="Gems"
+                    layout="fill"
+                    className=" object-cover "
+                />
+            </div>
+            <div className="container px-4 md:px-6 flex flex-col items-center justify-center text-center space-y-6 relative">
                 <div className="max-w-3xl  space-y-4">
                     <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
                         Loshan Gems
