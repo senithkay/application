62186f98bf1342218dd80b5b699d754ccc1bd788
Senith Uthsara <senithkarunarathneu@gmail.com>
2024-06-17 20:15:52 +0530

    integrate pagination with backend

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -2,10 +2,10 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function PaginationRounded() {
+export default function PaginationRounded({}) {
   return (
     <Stack spacing={2}>
-      <Pagination count={5} shape="rounded" />
+      <Pagination count={5}  shape="rounded" />
       
     </Stack>
   );
